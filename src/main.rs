314fd8acfc417c92059848c749662456fea1babd
wiseaidev <oss@wiseai.dev>
2025-04-09 15:56:07 +0300

    fix: only stage new files

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use gix::object::tree::EntryKind;
 use gix::objs;
 use gix::{open as open_repo, progress::Discard};
 use smallvec::SmallVec;
+use std::os::unix::ffi::OsStrExt;
 
 use self::cli::Cli;
 use crate::utils::{attempt_version_bump, get_current_version_from_config, read_files_from_config};
@@ -46,7 +47,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         } else {
             args.files
         };
-        let repo = open_repo(".")?;
+
+        let current_dir = std::env::current_dir()?;
+        let repo = open_repo(current_dir.to_str().unwrap())?;
 
         let statuses = repo.status(Discard)?;
         let mut changes = statuses.into_iter(Vec::<BString>::new())?;
@@ -83,6 +86,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         if commit {
             let mut entries = Vec::new();
+            let mut modified_files = Vec::new();
+
+            let mut head_ref = repo
+                .head_ref()?
+                .ok_or_else(|| anyhow!("No HEAD reference"))?;
+            let head_commit = head_ref.peel_to_commit()?;
+
+            let current_tree = head_commit.tree()?;
+            let current_entries = current_tree.iter().collect::<Result<Vec<_>, _>>()?;
 
             for path_str in &commit_files {
                 let path = Path::new(path_str);
@@ -95,19 +107,62 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     EntryKind::Blob.into()
                 };
 
-                entries.push(objs::tree::Entry {
-                    mode,
-                    filename: path
-                        .file_name()
-                        .ok_or_else(|| anyhow!("Invalid file name"))?
-                        .as_encoded_bytes()
-                        .to_vec()
-                        .into(),
-                    oid: blob_id.detach(),
-                });
+                let current_entry = current_entries
+                    .iter()
+                    .find(|e| e.filename() == path.file_name().unwrap().as_bytes());
+
+                if let Some(existing_entry) = current_entry {
+                    if *existing_entry.oid() != *blob_id {
+                        entries.push(objs::tree::Entry {
+                            mode,
+                            filename: path
+                                .file_name()
+                                .ok_or_else(|| anyhow!("Invalid file name"))?
+                                .as_encoded_bytes()
+                                .to_vec()
+                                .into(),
+                            oid: blob_id.detach(),
+                        });
+                        modified_files.push(path_str);
+                    } else {
+                        entries.push(objs::tree::Entry {
+                            mode: existing_entry.mode(),
+                            filename: existing_entry.filename().to_owned(),
+                            oid: existing_entry.oid().into(),
+                        });
+                    }
+                } else {
+                    entries.push(objs::tree::Entry {
+                        mode,
+                        filename: path
+                            .file_name()
+                            .ok_or_else(|| anyhow!("Invalid file name"))?
+                            .as_encoded_bytes()
+                            .to_vec()
+                            .into(),
+                        oid: blob_id.detach(),
+                    });
+                    modified_files.push(path_str);
+                }
+            }
+
+            for existing_entry in current_entries.iter() {
+                if !commit_files.contains(&existing_entry.filename().to_string()) {
+                    entries.push(objs::tree::Entry {
+                        mode: existing_entry.mode(),
+                        filename: existing_entry.filename().to_owned(),
+                        oid: existing_entry.oid().into(),
+                    });
+                }
+            }
+
+            if modified_files.is_empty() {
+                eprintln!("No files have been modified.");
+                return Ok(());
             }
 
             entries.sort_by(|a, b| a.filename.cmp(&b.filename));
+
             let tree = objs::Tree { entries };
             let tree_id = repo.write_object(&tree)?;
 
@@ -115,18 +170,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .replace("{current_version}", &current_version)
                 .replace("{new_version}", &new_version);
 
-            let mut head_ref = repo
-                .head_ref()?
-                .ok_or_else(|| anyhow!("No HEAD reference"))?;
-            let head_commit = head_ref.peel_to_commit()?;
-
             let signature = gix::actor::Signature {
                 name: head_commit.committer().unwrap().name.into(),
                 email: head_commit.committer().unwrap().email.into(),
                 time: gix::date::Time::now_utc(),
             };
+
             let commit = objs::Commit {
-                tree: tree_id.detach(),
+                tree: tree_id.into(),
                 parents: SmallVec::from_vec(vec![head_commit.id]),
                 author: signature.clone(),
                 committer: signature.clone(),
@@ -156,10 +207,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                 println!("Git lightweight tag created: refs/tags/{}", tag_name);
             }
+        } else {
+            eprintln!("No version bump attempted, and no files specified");
         }
-    } else {
-        eprintln!("No version bump attempted, and no files specified");
     }
-
     Ok(())
 }
